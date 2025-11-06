4aa6aca6efb0f34a533edf01930e90f7568d5af2
Chioma Princess Ohwo <ohwoprincess@gmail.com>
2025-11-06 14:25:29 +0100

    Implement welcome route in server.js
    Add welcome route for Task Manager API

--- server.js
+++ server.js
@@ -33,3 +33,7 @@ app.listen(PORT, () => {
   console.log(` Server is running on port ${PORT}`);
 });
 
+//server.js- main entry point for task manager api
+app.get('/', (req, res) => {
+  res.send('Welcome to Task Manager API');
+});
